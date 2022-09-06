eeb4c42499a095256448db26552fb9551a8a823f
Arthur Andrade <arthurandrade137@gmail.com>
2022-09-06 15:09:07 -0300

    :construction: Working in the play command

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,5 +1,8 @@
+
 import { iCommand } from "@src/interfaces/iCommand";
-import { Message } from "discord.js";
+import { CommandsCallError, CommandsInternalError } from "@src/model/CommandsError";
+import { Message, VoiceChannel } from 'discord.js';
+import { createAudioPlayer, DiscordGatewayAdapterCreator, joinVoiceChannel, NoSubscriberBehavior, VoiceConnection } from '@discordjs/voice';
 
 const play: iCommand = {
     name: 'play',
@@ -8,9 +11,23 @@ const play: iCommand = {
     aliases: ['p'],
     permission: [],
     cooldown: undefined,
-    active: false,
+    active: true,
     async execute(message: Message, args: string[]): Promise<void> {
-        
+        try {
+            if (!message.member!.voice.channel) throw new CommandsCallError(message, 'Você não está em um canal de voz');
+            const VoiceChannel = message.member?.voice.channel;
+            const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Play }});
+            const connection = joinVoiceChannel({
+                channelId: VoiceChannel!.id,
+                guildId: VoiceChannel!.guildId,
+                adapterCreator: VoiceChannel!.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator
+            });
+                        
+        } catch (error) {
+            if (error instanceof CommandsCallError) error.sendResponse();
+            if (error instanceof CommandsInternalError) error.logError();
+            console.log(error);
+        }
     }
 }
 export default play;
